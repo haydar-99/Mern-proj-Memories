53acdd627a8b29c4c93181433fa4f4f6eac6a184
haydar-99 <haydaralsaadi33@gmail.com>
2021-12-29 19:53:31 +0100

    create the codebase, server+ client side-conncetion to MongoDB

---
+++ server/index.js
@@ -0,0 +1,34 @@
+import express from "express";
+import bodyParser  from "body-parser";
+import  mongoose  from "mongoose";
+import cors from "cors";
+
+const app= express();
+
+app.use(bodyParser.json({limit: "30mb", extended: true}));
+app.use(bodyParser.urlencoded({limit: "30mb", extended: true}));
+app.use(cors());
+
+
+//DATABASE CONECCTION-CONFIG
+const CONNECTION_URL = 'mongodb+srv://mernProject:mernProject2021@cluster0.oclkj.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
+const PORT = process.env.PORT || 5000;
+
+mongoose.connect(CONNECTION_URL)
+.then(() => app.listen(PORT, () => console.log(`server is running on port: ${PORT}` )))
+.catch((error) => console.log(error.message));
+
+
+
+// const connectionParams={
+//     useNewUrlParser: true,
+//     // useCreateIndex: true,
+//     useUnifiedTopology: true 
+// }
+// mongoose.connect(CONNECTION_URL,connectionParams)
+//     .then( () => {
+//         console.log('Connected to database ')
+//     })
+//     .catch( (err) => {
+//         console.error(`Error connecting to the database. \n${err}`);
+//     })
